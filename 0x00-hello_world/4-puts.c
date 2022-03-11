084fabf0f7fa02c064174a48fa8087826e1f6dfb
edwinchris <edwinchris752@gmail.com>
2022-03-10 23:50:27 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-/*
- *main-prints the phrase programming is like building a multilingual puzzle.
- *return: 0 if exited properly, non-zero otherwise.
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
