e237ebbe1a191c4c04cdfd50b1575a13847df3a8
edwinchris <edwinchris752@gmail.com>
2022-03-15 16:06:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main-alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
