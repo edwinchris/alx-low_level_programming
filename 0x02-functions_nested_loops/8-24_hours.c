32cba626a1c5877f2277133fe8be10c5ab88e8e8
edwinchris <edwinchris752@gmail.com>
2022-03-15 18:30:49 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,20 +8,20 @@
  */
 void jack_bauer(void)
 {
-int hours_tens, hours_ones, minutes_tens, minute_ones,hours_max;
+int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 hours_max = 58;
 hours_tens = '0';
-while (hour_tens < '3')
+while (hours_tens < '3')
 {
 if (hours_tens == '2')
 {
 hours_max = '4';
 }
-hours_ones = '0'
+hours_ones = '0';
 while (hours_ones < hours_max)
 {
 minutes_tens = '0';
-while (minute_tens < '6')
+while (minutes_tens < '6')
 {
 minutes_ones = '0';
 while (minutes_ones < 58)
