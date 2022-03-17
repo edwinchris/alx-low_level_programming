ab59014619aad035f5c3680a5452b7e26d2164ee
edwinchris <edwinchris752@gmail.com>
2022-03-17 04:03:19 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 int i;
-printf("infinite loop incoming :(\n");
+printf("infinite loop incoming :\n");
 i = 0;
-/* while (i < 10)*/
+/*while (i < 10)*/
 /*{*/
 /*putchar(i);*/
 /*}*/
