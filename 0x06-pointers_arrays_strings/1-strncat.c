ee02af5a04fc8feaf2cf609d6868bb339260077a
edwinchris <edwinchris752@gmail.com>
2022-03-23 16:16:26 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
+ * _strncat - concatenates two strings
  * @dest: destination
  * @src: source
  * @n: amount of bytes used from src
@@ -15,8 +15,8 @@ count++;
 }
 while (count2 < n)
 {
-*(dest + count) = *(src + count2);
-if (*(src + count2) == '\0')
+*(dest + count) =  *(src + count2);
+if (*(src + count2) ==  '\0')
 break;
 count++;
 count2++;
