11a130a676038b296e438b7a637e6a097fd78c8c
edwinchris <edwinchris752@gmail.com>
2022-04-29 05:55:42 -0700

    my commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a linked list
+ * @head: List to free.
+ */
+void free_listint(listint_t *head)
+{
+listint_t *tmp;
+while (head != NULL)
+{
+tmp = (*head).next;
+free(head);
+head = tmp;
+}
+}
